a04cf6e908e0aff3efeb008d48fa0277ef83c575
CDGABZ <gabor+crescentdays@wearemindlabs.com>
2023-06-15 11:20:33 +0100

    Update

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template
+import openai
+
+openai.api_key = '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'
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def chat():
+    response = ""
+    if request.method == 'POST':
+        message = request.form.get('message')
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=message,
+            max_tokens=60
+        )
+        response = response['choices'][0]['text'].strip()
+    return render_template('chat.html', response=response)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
